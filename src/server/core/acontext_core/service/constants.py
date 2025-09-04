642e523c37900ed8c90cf2a224f864c192e816df
Gus <jianbaiye@outlook.com>
2025-09-04 11:26:24 +0800

    fix(core): remove unused rk

--- src/server/core/acontext_core/service/constants.py
+++ src/server/core/acontext_core/service/constants.py
@@ -4,5 +4,4 @@ class EX:
 
 class RK:
     session_message_insert = "session.message.insert"
-    session_message_buffer_delay = "session.message.buffer.delay"
     session_message_buffer_process = "session.message.buffer.process"
